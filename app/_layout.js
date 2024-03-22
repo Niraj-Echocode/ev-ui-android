459184b10afe2767a561b7f6cde57481f6c8cd96
Niraj-Echocode <156087349+Niraj-Echocode@users.noreply.github.com>
2024-03-22 19:51:31 +0530

    Update _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -1,15 +1,1 @@
-import { Stack } from "expo-router";
-import React from "react";
 
-const rootLayout = ({ children }) => {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        className="h-full flex justify-center items-center"
-        name="name"
-      />
-    </Stack>
-  );
-};
-
-export default rootLayout;
